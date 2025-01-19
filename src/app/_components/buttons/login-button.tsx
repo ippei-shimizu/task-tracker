eb6aff04e6e693fbdaa7c39720a13664e7ff4085
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2025-01-19 21:47:32 +0900

    add: Googleサインイン機能をログインボタンにクリックイベントを実装

--- src/app/_components/buttons/login-button.tsx
+++ src/app/_components/buttons/login-button.tsx
@@ -2,21 +2,31 @@
 
 import { auth, useGoogleAuth } from "@/hooks/user-google-auth";
 import { useRouter } from "next/navigation";
+import { useCallback } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 
 export default function LoginButton() {
   const [user, isLoading] = useAuthState(auth);
-  const { signOutWithGoogle } = useGoogleAuth();
+  const { signInWithGoogle, signOutWithGoogle } = useGoogleAuth();
   const router = useRouter();
 
-  const handleLogout = async () => {
+  const handleLogin = useCallback(async () => {
+    try {
+      await signInWithGoogle();
+      router.push("/top");
+    } catch (error) {
+      console.error(error);
+    }
+  }, [router, signInWithGoogle]);
+
+  const handleLogout = useCallback(async () => {
     try {
       await signOutWithGoogle();
       router.push("/");
     } catch (error) {
       console.error(error);
     }
-  };
+  }, [router, signOutWithGoogle]);
 
   if (isLoading) return <div className="h-10"></div>;
 
@@ -27,7 +37,9 @@ export default function LoginButton() {
           Logout
         </button>
       ) : (
-        <button className="text-black text-sm font-bold rounded-lg bg-buttonBg px-[10px] h-10">Login</button>
+        <button onClick={handleLogin} className="text-black text-sm font-bold rounded-lg bg-buttonBg px-[10px] h-10">
+          Login
+        </button>
       )}
     </>
   );
