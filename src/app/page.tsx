77d4da7d592f2f3c7df1c299e4847bd9012eea6a
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2025-01-19 13:04:11 +0900

    add: ホームページにログインセクションを追加し、Googleサインインボタンを実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <div className="w-11/12 max-w-[1184px] mx-auto py-5">
+        <h2 className="text-black text-2xl text-center font-bold">ログイン</h2>
+        <button className="block text-black text-sm font-bold mx-auto w-[448px] py-2 bg-buttonBg rounded-[12px] mt-4">
+          Sign in with Google
+        </button>
+      </div>
+    </main>
+  );
 }
