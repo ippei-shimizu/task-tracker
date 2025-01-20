e7e795b3a4c077e3c5f4de6a5817b3c54c71e67f
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2025-01-21 00:28:19 +0900

    add: タスク一覧表示のためのコンポーネントを追加

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -1,8 +1,22 @@
+import TaskLists from "@/app/tasks/_components/task-lists";
+import Link from "next/link";
+
 export default function Page() {
   return (
     <main>
-      <div className="w-11/12 max-w-[1184px] mx-auto py-5">
+      <div className="w-11/12 max-w-[960px] mx-auto py-5 mb-20">
         <h2 className="text-black text-2xl font-bold">タスク一覧</h2>
+        <div className="mt-8">
+          <TaskLists />
+        </div>
+        <div>
+          <Link
+            href="/task/new"
+            className="text-sm text-black text-center font-bold bg-buttonBg rounded-lg h-10 flex items-center justify-center max-w-[480px] mt-10"
+          >
+            Add task
+          </Link>
+        </div>
       </div>
     </main>
   );
