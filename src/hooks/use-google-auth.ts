fcbbd7db5b26f6909d19cbd6552943dcb7a44e72
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2025-01-23 23:18:54 +0900

    add: Googleサインイン後にタスクページへリダイレクトする機能を追加

--- src/hooks/use-google-auth.ts
+++ src/hooks/use-google-auth.ts
@@ -3,24 +3,28 @@ import { app } from "@/lib/firebase-config";
 import axios from "axios";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import Cookies from "js-cookie";
+import { useRouter } from "next/navigation";
+
 import { useCallback } from "react";
 
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
 
 export const useGoogleAuth = () => {
+  const router = useRouter();
   const signInWithGoogle = useCallback(async () => {
     try {
       const result = await signInWithPopup(auth, provider);
       const user = result.user;
       const accessToken = await user.getIdToken();
       await axios.post("/api/auth/login", { accessToken, userId: user.uid });
+      router.push("/tasks");
       return user;
     } catch (error) {
       console.error(error);
       throw error;
     }
-  }, []);
+  }, [router]);
 
   const signOutWithGoogle = useCallback(async () => {
     try {
